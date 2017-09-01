c2c646f25941a4d6d6d340a9af543bebd7ad29d2
fengzy1990 <fengzy2009@qq.com>
2017-09-01 18:59:06 +0800

    2017年9月1日18:58:49 上传了数据文件。ssm_curd.sql

---
+++ ssm_curd.sql
@@ -0,0 +1,49 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MyProject
+Source Server Version : 50514
+Source Host           : localhost:3306
+Source Database       : ssm_curd
+
+Target Server Type    : MYSQL
+Target Server Version : 50514
+File Encoding         : 65001
+
+Date: 2017-09-01 18:57:30
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `tbl_dept`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_dept`;
+CREATE TABLE `tbl_dept` (
+  `dept_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dept_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_dept
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_emp`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_emp`;
+CREATE TABLE `tbl_emp` (
+  `emp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `emp_name` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `d_id` int(11) NOT NULL,
+  PRIMARY KEY (`emp_id`,`d_id`),
+  KEY `fk_emp_dept` (`d_id`),
+  CONSTRAINT `fk_emp_dept` FOREIGN KEY (`d_id`) REFERENCES `tbl_dept` (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_emp
+-- ----------------------------
