fc19660c75cd04874c524306a38607b71a5c3055
fengzy1990 <fengzy2009@qq.com>
2017-09-06 22:48:49 +0800

    2017年9月6日22:48:17 mappertest文件，测试对数据库的删除  更新等操作。

--- src/main/java/com/fengzi/curd/test/MapperTest.java
+++ src/main/java/com/fengzi/curd/test/MapperTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.fengzi.curd.bean.Department;
 import com.fengzi.curd.bean.Employee;
+import com.fengzi.curd.bean.EmployeeExample;
+import com.fengzi.curd.bean.EmployeeExample.Criteria;
 import com.fengzi.curd.dao.DepartmentMapper;
 import com.fengzi.curd.dao.EmployeeMapper;
 
@@ -45,11 +47,29 @@ public class MapperTest {
 		// employeeMapper.insertSelective(new Employee(null, "zhangsan", "M",
 		// "test@fengzi.com", 1));
 		// 3.批量插入多个员工，使用可以执行批量操作的sqlSession
-		EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+		/*EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
 		for (int i = 0; i < 1000; i++) {
 			String uid = UUID.randomUUID().toString().substring(0, 5) + i;
 			mapper.insertSelective(new Employee(null, uid, "M", uid+"@fengzi.com", 1));
 		}
-		System.out.println("批量完成");
+		System.out.println("批量完成");*/
+		//4.测试部门删除
+		//departmentMapper.deleteByPrimaryKey(4);
+		//5.测试 部门的更新
+		//departmentMapper.updateByPrimaryKeySelective(new Department(3, "安全部"));
+		//6.测试员工删除，按照主键删除
+		//employeeMapper.deleteByPrimaryKey(1000);
+		//删除员工，按照样例删除，匹配任何职，如下。
+		/*EmployeeExample employeeExample=new EmployeeExample();
+		Criteria criteria=employeeExample.createCriteria();
+		criteria.andEmpNameEqualTo("zhangsan");
+		employeeMapper.deleteByExample(employeeExample);*/
+		//7.测试员工更新
+		/*Employee employee=new Employee();
+		employee.setEmpId(2);
+		employee.setEmpName("zhangsan");
+		employeeMapper.updateByPrimaryKeySelective(employee);
+		employeeMapper.updateByPrimaryKey(record);*/
+		//注意上面，updateByPrimaryKey是全量更新，所有字段必须齐全；updateByPrimaryKeySelective可以更新某一个字段。
 	}
 }
